07c23217e5d10394ccd110a79f4b2a68a36cf399
will Farrell <will.farrell@gmail.com>
2013-05-27 18:38:09 -0400

    pypi bug fix

--- Package Managers/pypi.php
+++ Package Managers/pypi.php
@@ -14,34 +14,29 @@ $cache = new Cache();
 $w = new Workflows();
 //$query = urlencode( "{query}" );
 
-// http://wiki.python.org/moin/PyPIXmlRpc
-if (strlen($query) >= $min_query_length) {
-	$pkgs = $cache->get_query_regex('pypi', $query, 'https://pypi.python.org/pypi?%3Aaction=search&term='.$query.'&submit=search', '/<tr class="(.*?)">([\s\S]*?)<\/tr>/i', 2);
+$pkgs = $cache->get_query_regex('pypi', $query, 'https://crate.io/?has_releases=on&q='.$query, '/<tr class="(.*?)">([\s\S]*?)<\/tr>/i', 2);
+
+$count = 25;
+foreach($pkgs as $item) {
+	// name
+	preg_match('/<a href="(.*?)">(.*?)<\/a>/i', $item, $matches);
+	$title = str_replace("&nbsp;", " ", strip_tags($matches[0]));
+	$url = strip_tags($matches[1]);
 	
-	$count = 25;
-	foreach($pkgs as $item) {
-		// name
-		preg_match('/<a href="(.*?)">(.*?)<\/a>/i', $item, $matches);
-		$title = str_replace("&nbsp;", " ", strip_tags($matches[0]));
-		$url = strip_tags($matches[1]);
-		
-		preg_match_all('/<td>([\s\S]*?)<\/td>/i', $item, $matches);
-		$details = strip_tags($matches[1][2]);
-		
-		$w->result( $title, 'https://pypi.python.org'.$url, $title, $details, 'icon-cache/pypi.png' );
-		if (!--$count) { break; }
-	}
+	preg_match_all('/<td>([\s\S]*?)<\/td>/i', $item, $matches);
+	$details = strip_tags($matches[1][2]);
 	
-	if ( count( $w->results() ) == 0) {
-		if($query) {
-			$w->result( 'pypi', 'https://pypi.python.org/pypi?%3Aaction=search&term='.$query.'&submit=search', 'No packages were found that matched "'.$query.'"', 'Click to see the results for yourself', 'icon-cache/pypi.png' );
-		}
-		$w->result( 'pypi-www', 'http://rubygems.org/', 'Go to the website', 'http://rubygems.org', 'icon-cache/pypi.png' );
-	}
-} else {
-	$w->result( 'pypi', null, 'Query too short', 'Due to drawbacks in the API, the minimum query length is '.$min_query_length.'.', 'icon-cache/pypi.png', 'no' );
-	$w->result( 'pypi-www', 'http://rubygems.org/', 'Go to the website', 'http://rubygems.org', 'icon-cache/pypi.png' );
+	$w->result( $title, 'https://pypi.python.org'.$url, $title, $details, 'icon-cache/pypi.png' );
+	if (!--$count) { break; }
 }
+
+if ( count( $w->results() ) == 0) {
+	if($query) {
+		$w->result( 'pypi', 'https://crate.io/?has_releases=on&q='.$query, 'No packages were found that matched "'.$query.'"', 'Click to see the results for yourself', 'icon-cache/pypi.png' );
+	}
+	$w->result( 'pypi-www', 'https://pypi.python.org/', 'Go to the website', 'https://pypi.python.org', 'icon-cache/pypi.png' );
+}
+
 
 echo $w->toxml();
 // ****************
